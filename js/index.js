fa160369982216cc4c58cec68b16745314d3f365
Sami ul Haq <samiulhaq1899@gmail.com>
2020-08-27 18:35:38 +0500

    main page completed

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let menuItems = document.getElementById('menuItems');
+
+menuItems.style.maxHeight = "0px";
+
+function menuToggle() { 
+    if(menuItems.style.maxHeight == "0px"){
+        menuItems.style.maxHeight = "500px";
+    }
+    else{
+        menuItems.style.maxHeight = "0px";
+    }
+}
