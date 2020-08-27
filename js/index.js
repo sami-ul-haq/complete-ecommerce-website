1e93e20adf7c4a90928b0c836b0dcf994ab76254
Sami ul Haq <samiulhaq1899@gmail.com>
2020-08-27 20:20:37 +0500

    added more pages

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ function menuToggle() {
         menuItems.style.maxHeight = "0px";
     }
 }
+
+// Js Product Detail Gallery 
+
+let productImg = document.getElementById('productImg');
+
+let smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function(){
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    productImg.src = smallImg[3].src;
+}
